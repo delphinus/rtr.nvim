415d68af0e1d33d2708f88d9d5dc6c54c88d504b
JINNOUCHI Yasushi <me@delphinus.dev>
2025-03-11 15:52:42 +0900

    refactor: do not use OOP (#9)

--- lua/rtr.lua
+++ lua/rtr.lua
@@ -1,37 +1,31 @@
 local config = require "rtr.config"
 
 ---@class rtr.Rtr
----@field augroup_name string
-local Rtr = {}
-
----@return rtr.Rtr
-Rtr.new = function()
-  return setmetatable({ augroup_name = "rtr" }, { __index = Rtr })
-end
+local M = {
+  augroup_name = "rtr",
+}
 
 ---@return nil
-function Rtr:setup()
+function M.setup()
   ---@return boolean
   local function can_work()
     return not not (vim.fs and vim.fs.root)
   end
 
   if not can_work() then
-    self:notify("This plugin needs vim.fs.root", vim.log.levels.ERROR)
+    M.notify("This plugin needs vim.fs.root", vim.log.levels.ERROR)
     return
   end
 
   vim.api.nvim_create_autocmd("BufWinEnter", {
-    group = vim.api.nvim_create_augroup(self.augroup_name, {}),
-    callback = function(ev)
-      self:on_buf_win_enter(ev)
-    end,
+    group = vim.api.nvim_create_augroup(M.augroup_name, {}),
+    callback = M.on_buf_win_enter,
   })
 end
 
 ---@param ev vim.api.keyset.create_autocmd.callback_args
-function Rtr:on_buf_win_enter(ev)
-  if not self:is_file(ev.buf) then
+function M.on_buf_win_enter(ev)
+  if not M.is_file(ev.buf) then
     return
   end
   if config.disabled_filetypes then
@@ -44,16 +38,16 @@ function Rtr:on_buf_win_enter(ev)
   end
   local root = vim.fs.root(ev.buf, config.root_names)
   if not root then
-    self:notify(("cannot find root for buffer:  %d"):format(ev.buf))
+    M.notify(("cannot find root for buffer:  %d"):format(ev.buf))
     return
   end
   vim.cmd.lcd(root)
-  self:notify("Set CWD to " .. root)
+  M.notify("Set CWD to " .. root)
 end
 
 ---@param bufnr integer
 ---@return boolean
-function Rtr:is_file(bufnr)
+function M.is_file(bufnr)
   if not config.enabled_buftypes then
     return false
   end
@@ -62,20 +56,17 @@ end
 
 ---@param msg string
 ---@param level integer?
-function Rtr:notify(msg, level)
+function M.notify(msg, level)
   local log_level = level or config.log_level
   if log_level then
     vim.notify("[rtr] " .. msg, log_level)
   end
 end
 
-local rtr = Rtr.new()
-
 return {
   ---@param opts rtr.Opts?
   setup = function(opts)
     config.setup(opts)
-    rtr:setup()
+    M.setup()
   end,
-  rtr = rtr,
 }
